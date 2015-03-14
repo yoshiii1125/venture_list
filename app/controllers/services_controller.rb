0ae65dc81cdf9dee3e99ca27f1ddb86b9f2ce5e1
konumashuri <konuma@inno-beta.com>
2015-03-14 23:59:22 +0900

    Install Nokogiri

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,15 @@
 class ServicesController < ApplicationController
   def index
+    require 'open-uri'
+    url = 'http://www.seed-db.com/accelerators/view?acceleratorid=1011'
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    @services = doc.css('table#seedcos tbody tr').first
   end
 
   def show

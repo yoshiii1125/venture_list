3538cfd6c11d8c0bcef52360c5b5dde406c1e49b
konumashuri <konuma@inno-beta.com>
2015-03-15 15:33:05 +0900

    Prepare db tables

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,3 @@
 class Resource < ActiveRecord::Base
+  validates_presence_of :name, :web_url
 end
--- app/models/service_has_category.rb
+++ app/models/service_has_category.rb
@@ -1,2 +1,3 @@
 class ServiceHasCategory < ActiveRecord::Base
+  validates_presence_of :service_id, :category_id
 end
